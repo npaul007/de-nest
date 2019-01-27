60acccbe03ad306d1af1fa742cf2f6ab51152d9e
nate <nate@Macs-Mac-Pro-380.local>
2019-01-27 18:20:11 -0500

    started foundation of library, can now set tagName

--- de-nest.js
+++ de-nest.js
@@ -1,1 +1,26 @@
-console.log("foo");
+let denest = (element,options) => 
+{
+    let newElement;
+
+    // handling tagNames
+    if(options && options.tagName)
+    {
+        newElement = document.createElement(options.tagName.toLowerCase());
+    }
+    else 
+    {
+        newElement = document.createElement(element.tagName.toLowerCase());
+    }
+
+    console.log(newElement);
+
+    while(element && element.children)
+    {
+       if(element && element.children)
+       {
+           element = element.children[0];
+       }
+    }
+
+    return newElement;
+}
