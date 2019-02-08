637eb9e96379816ccb3aae072114f6a8a81fe092
nate <npaulemon@gmail.com>
2019-02-07 20:22:18 -0500

    moved some files to different folders, added testing code framework for module

---
+++ test/index.js
@@ -0,0 +1,13 @@
+
+console.log("***Commencing tests for de-nest module***");
+
+
+var assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
